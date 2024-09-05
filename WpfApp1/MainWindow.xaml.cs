ae57b57f9fbbccf9f42cbd67972d2e1a624f1717
Ilya Karasev <97747062+faermot@users.noreply.github.com>
2024-09-05 23:29:55 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+        }
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string userLogin = "admin";
+            string userPassword = "admin1234";
+
+            if (string.IsNullOrEmpty(TbLogin.Text) && string.IsNullOrEmpty(PbPSW.Password))
+            {
+                MessageBox.Show("Логин или пароль неверный!");
+
+            }
+            else
+            {
+                if (TbLogin.Text == userLogin && PbPSW.Password == userPassword)
+                {
+                    MessageBox.Show("Вы успешно авторизованны!");
+                }
+                else
+                {
+                    MessageBox.Show("Произошла неизвестная ошибка!");
+                }
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
